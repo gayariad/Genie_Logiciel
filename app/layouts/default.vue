2ee4ffb27679b6d2ec9923dfdab2d42cb2566965
quent <quentinflageo@hotmail.com>
2026-03-16 08:31:52 +0100

    ajout de la page favoris et de la page watchlist

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -56,18 +56,18 @@
         </svg>
       </NuxtLink>
 
-      <div class="sidebar-icon" title="WatchList">
+      <NuxtLink to="/watchlist" class="sidebar-icon" title="WatchList">
         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
           <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.644C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
           <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
         </svg>
-      </div>
+      </NuxtLink>
 
-      <div class="sidebar-icon" title="Favoris">
+      <NuxtLink to="/favoris" class="sidebar-icon" title="Favoris">
         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
         </svg>
-      </div>
+      </NuxtLink>
 
       <div class="sidebar-icon" title="Stats">
         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
